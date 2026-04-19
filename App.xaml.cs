7ebdb501f4c67fc6876d6dae3f117fbd47a8faa2
Mihai <mihai.marinescu2112@yahoo.com>
2026-04-19 19:38:54 +0300

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Turn_One_Link;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
